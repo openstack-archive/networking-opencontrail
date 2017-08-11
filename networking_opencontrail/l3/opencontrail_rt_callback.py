025f65f297edcb5b6a65ad2d6b7cd49271bfbfa8
Piotr Zalas <piotr.zalas@codilime.com>
2017-08-11 09:35:09 +0200

    Add support for router configuration interchange
    Adds hooks for monitoring router status changes.
    Ties these hooks with driver back-end, so now
    actions related to routers are executed on Contrail
    node.
    
    Change-Id: Id373b7178f0ad8eea99b22298805bb0618e6d3cf

---
+++ networking_opencontrail/l3/opencontrail_rt_callback.py
@@ -0,0 +1,168 @@
+# Copyright (c) 2017 OpenStack Foundation
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+from neutron.db import api as db_api
+from neutron.db import common_db_mixin
+from neutron.db import extraroute_db
+from neutron.db import l3_agentschedulers_db
+from neutron.db import l3_dvr_db
+from neutron.db import l3_gwmode_db
+from neutron_lib.plugins import constants as plugin_constants
+from oslo_log import helpers as log_helpers
+from oslo_log import log as logging
+from oslo_utils import excutils
+
+import networking_opencontrail.drivers.drv_opencontrail as driver
+
+LOG = logging.getLogger(__name__)
+
+
+class OpenContrailRouterHandler(common_db_mixin.CommonDbMixin,
+                                extraroute_db.ExtraRoute_db_mixin,
+                                l3_dvr_db.L3_NAT_with_dvr_db_mixin,
+                                l3_gwmode_db.L3_NAT_db_mixin,
+                                l3_agentschedulers_db.L3AgentSchedulerDbMixin):
+    """Router Handler for OpenContrail networking.
+
+    Registers for the notification of router updates.
+    """
+    supported_extension_aliases = ["dvr", "router", "ext-gw-mode",
+                                   "extraroute"]
+
+    def __init__(self):
+        super(OpenContrailRouterHandler, self).__init__()
+        self.driver = driver.OpenContrailDrivers()
+
+    def get_plugin_type(self):
+        return plugin_constants.L3
+
+    def get_plugin_description(self):
+        """Returns string description of the plugin."""
+        return ("L3 Router Service Plugin for basic L3 forwarding "
+                "using OpenContrail.")
+
+    @log_helpers.log_method_call
+    def create_router(self, context, router):
+        """Create Router callback handler for OpenContrail networking.
+
+        Invokes back-end driver to create router in OpenContrail.
+        """
+        try:
+            router = self.driver.create_router(context, router)
+            session = db_api.get_writer_session()
+            with session.begin(subtransactions=True):
+                super(OpenContrailRouterHandler,
+                      self).create_router(context, {'router': router})
+        except Exception as e:
+            with excutils.save_and_reraise_exception():
+                LOG.error("Failed to create a router %(id)s: %(err)s",
+                          {"id": router["id"], "err": e})
+                try:
+                    self.driver.delete_router(context, router['id'])
+                except Exception:
+                    LOG.exception("Failed to delete router %s",
+                                  router['id'])
+
+        return router
+
+    @log_helpers.log_method_call
+    def delete_router(self, context, router_id):
+        """Delete Router callback handler for OpenContrail networking.
+
+        Invokes back-end driver to delete router from OpenContrail.
+        """
+        session = db_api.get_writer_session()
+        with session.begin(subtransactions=True):
+            super(OpenContrailRouterHandler, self).delete_router(context,
+                                                                 router_id)
+
+            try:
+                self.driver.delete_router(context, router_id)
+            except Exception as e:
+                LOG.error("Failed to delete router %(id)s: %(err)s",
+                          {"id": router_id, "err": e})
+
+    @log_helpers.log_method_call
+    def update_router(self, context, router_id, router):
+        """Update Router callback handler for OpenContrail networking.
+
+        Invokes back-end driver to update router in OpenContrail.
+        """
+        if 'name' in router['router']:
+            LOG.warning("OpenContrail doesn't allow for changing router "
+                        "%(id)s name to %(name)s",
+                        {"id": router_id, "name": router['router']['name']})
+
+        session = db_api.get_writer_session()
+        with session.begin(subtransactions=True):
+            router_dict = super(OpenContrailRouterHandler, self).update_router(
+                context, router_id, router)
+
+            try:
+                self.driver.update_router(context, router_id, router)
+            except Exception as e:
+                LOG.error("Failed to update router %(id)s: %(err)s",
+                          {"id": router_id, "err": e})
+
+        return router_dict
+
+    @log_helpers.log_method_call
+    def add_router_interface(self, context, router_id, interface_info):
+        """Add Router Interface callback handler for OpenContrail.
+
+        Invokes back-end driver to add router interface in OpenContrail.
+        """
+        session = db_api.get_writer_session()
+        with session.begin(subtransactions=True):
+            new_router = super(
+                OpenContrailRouterHandler, self).add_router_interface(
+                    context, router_id, interface_info)
+
+        try:
+            self.driver.add_router_interface(context, router_id,
+                                             interface_info)
+        except Exception as e:
+            with excutils.save_and_reraise_exception():
+                LOG.error("Failed to add interface to router %(id)s: "
+                          "%(err)s", {"id": router_id, "err": e})
+                try:
+                    self.remove_router_interface(context, router_id,
+                                                 interface_info)
+                except Exception:
+                    LOG.exception("Failed to delete interface of router %s",
+                                  router_id)
+
+        return new_router
+
+    @log_helpers.log_method_call
+    def remove_router_interface(self, context, router_id, interface_info):
+        """Remove router interface callback handler for OpenContrail.
+
+        Invokes back-end driver to remove router interface from OpenContrail.
+        """
+        session = db_api.get_writer_session()
+        with session.begin(subtransactions=True):
+            new_router = super(
+                OpenContrailRouterHandler, self).remove_router_interface(
+                    context, router_id, interface_info)
+
+        try:
+            self.driver.remove_router_interface(context, router_id,
+                                                interface_info)
+        except Exception as e:
+            LOG.error("Failed to remove router %(id)s interface: "
+                      "%(err)s", {"id": router_id, "err": e})
+
+        return new_router
