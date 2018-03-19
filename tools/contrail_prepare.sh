f59c607b523e1a40b166004c3aeaf712412ca66f
Kamil Renczewski <kamil.renczewski@codilime.com>
2018-03-19 15:22:39 +0100

    Zuul job
    Change-Id: I01c25e809662eddcb0404b79be8b640377622dd2

---
+++ tools/contrail_prepare.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "RUN: $0 with args $*"
+
+OPENSTACK_IP="$1"
+echo "OpenStack IP = $OPENSTACK_IP"
+
+install_prereq()
+{
+	sudo yum remove -y docker docker-common docker-selinux docker-engine
+	sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+	sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+	sudo yum -y install docker-ce
+	sudo groupadd docker
+	sudo usermod -aG docker $USER
+	sudo service docker start
+
+	sudo yum -y install epel-release
+	sudo yum -y update
+	sudo yum -y install python-pip docker-compose ansible
+	sudo pip install docker==3.0.0 docker-py docker-compose
+
+	# Dirty ugly workaround on contrail-ansible-deployer not using become in playbooks
+	sudo mkdir -p /var/log/contrail
+	sudo mkdir -p /etc/contrail
+	sudo chmod 777 /var/log/contrail /etc/contrail
+	# End of workaround
+}
+
+install_prereq
+
