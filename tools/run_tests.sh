f50dddbeac0116b1907e52354261a4e5bcd5ec2d
Kamil Renczewski <kamil.renczewski@codilime.com>
2018-03-29 13:12:56 +0200

    Integration testing draft
    Change-Id: Ibdb2f834c05c7d74d2f59d106026229712178341

--- tools/run_tests.sh
+++ tools/run_tests.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+CONTRAIL_IP="$1"
+OPENSTACK_IP="$2"
+
 uname -a
 cat /etc/redhat-release
+cat /etc/os-release
 echo 'Test OK'
+echo "Contrail is: $CONTRAIL_IP; OpenStack is: $OPENSTACK_IP;"
+
+curl "http://$CONTRAIL_IP:8082/virtual-networks"
+
