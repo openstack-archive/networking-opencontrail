4225b660243a47d1864d3f38e1e5ae242c5167a6
Kamil Renczewski <kamil.renczewski@codilime.com>
2018-03-29 13:12:56 +0200

    Integration testing draft
    Change-Id: Ibdb2f834c05c7d74d2f59d106026229712178341

--- tools/contrail_prepare.sh
+++ tools/contrail_prepare.sh
@@ -18,11 +18,6 @@ install_prereq()
 	sudo yum -y install epel-release
 	sudo yum -y update
 	sudo yum -y install python-pip
-	# Need docker python library in version 2.6.1 until https://github.com/ansible/awx/issues/1197 is fixed
-	# Also need to use docker-compose 1.15 because of: https://github.com/docker/compose/issues/5156
-	sudo pip install docker==2.6.1 docker-compose==1.15.0
-	# docker-py is not installed here as commented here: https://github.com/ansible/ansible/issues/37958#issuecomment-376320961
-
 	sudo pip install -U docker docker-compose
 
 	# Dirty ugly workaround on contrail-ansible-deployer not using become in playbooks
