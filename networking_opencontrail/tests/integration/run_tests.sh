9e31e4c57351eb215eb5b0d639df0fb1088af753
Kamil Renczewski <kamil.renczewski@codilime.com>
2018-04-13 21:25:42 +0200

    Testing&fixing zuul job
    Change-Id: I42150c0017dde3134d0b05be70ef6ea23262e2bf

---
+++ networking_opencontrail/tests/integration/run_tests.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo "Integraion testing with opencontrail"
+
+OPENSTACK_IP="$1"
+CONTRAIL_IP="$2"
+echo "Contrail is: $CONTRAIL_IP; OpenStack is: $OPENSTACK_IP;"
+
+set -x 
+
+cat /opt/stack/devstack/userrc_early
+source /opt/stack/devstack/userrc_early
+
+openstack project list
+openstack network list
+curl -sk "http://$CONTRAIL_IP:8082/virtual-networks"
+
+Response=$(openstack network create -f shell --project=demo spock)
+echo "Spock create response: $Response"
+SpockID=$(echo "$Response" | sed -n '/^id/ s/^.*="\([^"]\+\)"/\1/; T; p;')
+SpockContrail=$(curl -sk "http://$CONTRAIL_IP:8082/virtual-networks")
+echo "$SpockContrail" | grep "$SpockID"
