3a936b7a8442313c787b18a5575113c9cba81ae2
Kamil Renczewski <kamil.renczewski@codilime.com>
2018-04-13 21:25:42 +0200

    Testing&fixing zuul job
    Change-Id: I42150c0017dde3134d0b05be70ef6ea23262e2bf

---
+++ networking_opencontrail/tests/integration/run_tests.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+echo "Integraion testing with opencontrail"
+
+OPENSTACK_IP="$1"
+CONTRAIL_IP="$2"
+echo "Contrail is: $CONTRAIL_IP; OpenStack is: $OPENSTACK_IP;"
+
+set -x 
+
+pgrep keystone
+echo "Port 5000"
+curl -sk https://127.0.0.1:5000
+echo "Port 35357"
+curl -sk https://127.0.0.1:35357
+
+export OS_USERNAME=admin
+export OS_PASSWORD=admin
+export OS_PROJECT_NAME=admin
+export OS_PROJECT_DOMAIN_ID=default
+export OS_USER_DOMAIN_ID=default
+export OS_IDENTITY_API_VERSION=3
+export OS_AUTH_URL=http://127.0.0.1:5000/v2.0/
+
+openstack network list
+curl -sk "http://$CONTRAIL_IP:8082/virtual-networks"
+
+SpockID=$(openstack network create --project=demo spock | sed -n '/^id/ s/^.*="\([^"]\+\)"/\1/; T; p;')
+curl -sk "http://$CONTRAIL_IP:8082/virtual-networks" | grep "$SpockID"
