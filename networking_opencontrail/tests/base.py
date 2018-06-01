bd123d85036fe01a42a37bf8c750480d45db8de3
Paweł Zadrożny <pawel.zadrozny@codilime.com>
2018-06-01 15:28:52 +0200

    Integration test suite
    Change-Id: I51be6dfc90a94d7d8fbec379ba82b9474181d817

--- networking_opencontrail/tests/base.py
+++ networking_opencontrail/tests/base.py
@@ -14,10 +14,31 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
+import os
 
+from keystoneauth1 import identity
+from keystoneauth1 import session
+from neutronclient.v2_0 import client as neutron
 from oslotest import base
 
 
 class TestCase(base.BaseTestCase):
-
     """Test case base class for all unit tests."""
+
+
+class IntegrationTestCase(base.BaseTestCase):
+    """Base test case for all integration tests."""
+
+    def setUp(self):
+        super(IntegrationTestCase, self).setUp()
+        controller_ip = os.getenv('CONTROLLER_IP', 'localhost')
+        self.contrail_api = 'http://{}:8082'.format(controller_ip)
+
+        auth_url = 'http://{}:5000/identity/v3'.format(controller_ip)
+        auth = identity.V3Password(auth_url=auth_url,
+                                   username='admin', password='admin',
+                                   project_name='demo',
+                                   project_domain_id='default',
+                                   user_domain_id='default')
+        sess = session.Session(auth=auth)
+        self.neutron = neutron.Client(session=sess)
