df4156c05a3cbc9da974d89df4db72fe3d231bd9
Maciej Jagiello <maciej.jagiello@codilime.com>
2018-07-27 12:58:22 +0200

    Add devstack-tempest job
    We want to run all network releated tempest tests to validate
    if plugin works correctly.
    Because there was no tempest tests before, I expect them to
    fail which will be ok on this stage.
    This is why all noc-tempest* tests are set to non-voting.
    
    Change-Id: I44c5c12bbbaabb75f80f38ba7fb6ddaccc93fbca

--- networking_opencontrail/tests/integration/test_manage_network.py
+++ networking_opencontrail/tests/integration/test_manage_network.py
@@ -18,47 +18,35 @@ from networking_opencontrail.tests.base import IntegrationTestCase
 
 
 class TestManageNetwork(IntegrationTestCase):
-    def setUp(self):
-        super(TestManageNetwork, self).setUp()
-        self.api_virtual_nets = '{}/virtual-networks'.format(self.contrail_api)
-
-    @staticmethod
-    def find_network(name, networks):
-        """Iterate over virtual-networks list obtained from OpenContrail API.
-
-        Lookup for network name in fq_name list.
-        """
-        for net in networks:
-            if name in net['fq_name']:
-                result = requests.get(net['href']).json()
-                return result['virtual-network']
-        return None
-
-    def test_contrail_synchronized_projects(self):
-        pass
-
     def test_create_vlan_network(self):
         """Create vlan network using openstack CLI.
 
         Check if network exists in OpenContrail and all properties are
         the same as used in create command call.
         """
-        network = {
-            'name': 'test_public_1',
-            'provider:network_type': 'vlan',
-            'provider:segmentation_id': 10,
-            'provider:physical_network': 'public',
-            'admin_state_up': True
-        }
-        self.neutron.create_network({'network': network})
+        try:
+          ps = requests.get('{}/projects'.format(self.contrail_api)) 
+          print(ps.text)
+        except:
+          pass
+        network = self.neutron.create_network({
+            'network': {
+                'name': 'test_public_1',
+                'provider:network_type': 'vlan',
+                'provider:segmentation_id': 10,
+                'provider:physical_network': 'public',
+                'admin_state_up': True,
+            },
+        })
+        print(network) 
+        print(dir(network)) 
+        url = '{}/virtual-network/{}'.format(self.contrail_api, network['network']['id'])
+        res = requests.get(url)
+        self.assertTrue(res.status_code in [200, 301, 302, 303, 304])
 
-        result = requests.get(self.api_virtual_nets).json()
-        check_network = self.find_network(network['name'],
-                                          result['virtual-networks'])
-
-        self.assertIsNotNone(check_network)  # check if network exists first
-        self.assertEqual(check_network['name'], network['name'])
+        result = res.json().get('virtual-network')
+        
         self.assertDictEqual(
-            check_network['provider_properties'],
-            {'segmentation_id': network['provider:segmentation_id'],
-             'physical_network': network['provider:physical_network']})
+            result['provider_properties'],
+            {'segmentation_id': network['network']['provider:segmentation_id'],
+             'physical_network': network['network']['provider:physical_network']})
