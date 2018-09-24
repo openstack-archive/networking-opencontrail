c11ca9b55bba0243a0b67f93faacd1e35f34f625
Krzysztof Nagrodkiewicz <krzysztof.nagrodkiewicz@codilime.com>
2018-09-24 11:35:14 +0200

    Add test for creating network and subnet
    Author:    Krzysztof Nagrodkiewicz <knagrodkiewicz@gmail.com>
    
    Change-Id: I81817e7b2a021c49f5950f706c25b2ecb3c821eb

--- networking_opencontrail/tests/integration/test_manage_network.py
+++ networking_opencontrail/tests/integration/test_manage_network.py
@@ -18,7 +18,27 @@ from networking_opencontrail.tests.base import IntegrationTestCase
 
 
 class TestManageNetwork(IntegrationTestCase):
+
     def test_create_vlan_network(self):
+        network = self.neutron.create_network({
+            'network': {
+                'name': 'test_public_1',
+                'provider:network_type': 'vlan',
+                'provider:segmentation_id': 123,
+                'provider:physical_network': 'public',
+                'admin_state_up': True,
+            },
+        })
+
+        contrail_network = self.contrail_network_get(network['network']['id'])
+        expected_network_subdict = {
+            'segmentation_id': network['network']['provider:segmentation_id'],
+            'physical_network': network['network']['provider:physical_network']
+        }
+        self.assertDictEqual(contrail_network['provider_properties'],
+                             expected_network_subdict)
+
+    def test_create_vlan_network_and_subnet(self):
         """Create vlan network using openstack CLI.
 
         Check if network exists in OpenContrail and all properties are
@@ -26,25 +46,35 @@ class TestManageNetwork(IntegrationTestCase):
         """
         network = self.neutron.create_network({
             'network': {
-                'name': 'test_public_1',
+                'name': 'test_public_2',
                 'provider:network_type': 'vlan',
-                'provider:segmentation_id': 10,
+                'provider:segmentation_id': 199,
                 'provider:physical_network': 'public',
                 'admin_state_up': True,
             },
         })
-        contrail_network = self.contrail_network_get(network['network']['id'])
+        subnet = self.neutron.create_subnet({
+            'subnet': {
+                'name': 'test_subnet_1',
+                'cidr': '192.168.2.0/24',
+                'network_id': network['network']['id'],
+                'gateway_ip': None,
+                'ip_version': 4,
+            }
+        })
 
-        expected_network_subdict = {
-            'segmentation_id': network['network']['provider:segmentation_id'],
-            'physical_network': network['network']['provider:physical_network']
-        }
-
-        self.assertDictEqual(contrail_network['provider_properties'],
-                             expected_network_subdict)
+        contrail_subnet = self.contrail_subnet_get(network['network']['id'])
+        self.assertIsNotNone(contrail_subnet)
+        self.assertEqual(subnet['subnet']['name'], contrail_subnet['subnet_name'])
 
     def contrail_network_get(self, network_id):
         url = '{}/virtual-network/{}'.format(self.contrail_api, network_id)
         network_result = requests.get(url)
-        self.assertIn(network_result.status_code, {200, 301, 302, 303, 304})
+        self.assertIn(network_result.status_code, [200, 301, 302, 303, 304])
         return network_result.json().get('virtual-network')
+
+    def contrail_subnet_get(self, network_id):
+        url = '{}/virtual-network/{}'.format(self.contrail_api, network_id)
+        network_result = requests.get(url)
+        self.assertIn(network_result.status_code, [200, 301, 302, 303, 304])
+        return network_result.json().get('network_ipam_refs')[0].get('attr').get('ipam_subnets')[0].get('subnet')
