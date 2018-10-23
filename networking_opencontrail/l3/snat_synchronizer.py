6018a6493c65d8fedc114de563478c2a2364bd7b
Adrian Szczepański <adrian.szczepanski@codilime.com>
2018-10-23 16:10:08 +0200

    Create port for snat
    Create port for SNAT to prevent IP address collisions
    Add SNAT handling in L3 flavor
    
    Co-authored-by: Kamil Mankowski <kamil.mankowski@codilime.com>
    Co-authored-by: Maciej Jagiello <maciej.jagiello@codilime.com>
    
    Change-Id: I7c0cbbd9f3c5045d31f9bb66450552a08f424f32

---
+++ networking_opencontrail/l3/snat_synchronizer.py
@@ -0,0 +1,158 @@
+# Copyright (c) 2017 OpenStack Foundation
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+from requests import codes as http_status
+
+from neutron_lib.plugins import directory
+from retrying import retry
+
+import networking_opencontrail.drivers.rest_driver as rest_driver
+
+TF_SNAT_DEVICE_OWNER = 'tf-compatibility:snat'
+
+
+class SnatSynchronizer(object):
+    def __init__(self):
+        self.tf_helper = TFHelper()
+        self.neutron_helper = NeutronHelper()
+
+    def sync_snat_interfaces(self, context, router_id, router=None):
+        new_snat_ips = self._get_tf_snat_ips(router_id, router)
+
+        neutron_interfaces = self.neutron_helper.get_snat_interfaces(context,
+                                                                     router_id)
+        for interface in neutron_interfaces:
+            # This interfaces were created by plugin with only one IP
+            ip_address = interface['fixed_ips'][0]['ip_address']
+            if ip_address not in new_snat_ips:
+                self.neutron_helper.delete_snat_interface(context, interface)
+            else:
+                new_snat_ips.remove(ip_address)
+
+        for new_ip in new_snat_ips:
+            self.neutron_helper.create_snat_interface(
+                context, router['external_gateway_info'], router_id, new_ip)
+
+    def _get_tf_snat_ips(self, router_id, router):
+        if (router and 'external_gateway_info' in router
+                and router['external_gateway_info']):
+            return self.tf_helper.get_snat_ips(router_id)
+        return []
+
+
+class NeutronHelper(object):
+    def get_snat_interfaces(self, context, router_id):
+        ports = self._core_plugin.get_ports(
+            context, filters={'device_id': [router_id],
+                              'device_owner': [TF_SNAT_DEVICE_OWNER]})
+        return ports
+
+    def create_snat_interface(self, context, external_gateway_info,
+                              router_id, snat_ip):
+        port_data = {'tenant_id': context.tenant_id,
+                     'network_id': external_gateway_info['network_id'],
+                     'fixed_ips': [{'ip_address': snat_ip}],
+                     'device_id': router_id,
+                     'device_owner': TF_SNAT_DEVICE_OWNER,
+                     'admin_state_up': True,
+                     'name': 'tungstenfabric-snat-interface'}
+        port = self._core_plugin.create_port(context, {'port': port_data})
+        return port
+
+    def delete_snat_interface(self, context, snat_interface):
+        self._core_plugin.delete_port(context, snat_interface['id'])
+
+    @property
+    def _core_plugin(self):
+        return directory.get_plugin()
+
+
+class TFHelper(object):
+    def __init__(self):
+        self.rest_driver = rest_driver.ContrailRestApiDriver()
+
+    def get_snat_ips(self, router_id):
+        router = self._get_router(router_id)
+        if not router:
+            return []
+
+        instance_ip_uuids = set(self._get_snat_ip_refs(router))
+        snat_ips = set(self._get_instance_ip(iip_id)['instance_ip_address']
+                       for iip_id in instance_ip_uuids)
+
+        return list(snat_ips)
+
+    # TF creates snat async, so max. 3 retry to try get it
+    # Key error usually means that snat interface is not ready yet
+    @retry(retry_on_exception=lambda exc: isinstance(exc, KeyError),
+           stop_max_attempt_number=3,
+           wait_fixed=1000)
+    def _get_snat_ip_refs(self, router):
+        for service_ref in router['service_instance_refs']:
+            if self._not_a_snat_service(service_ref):
+                continue
+            service = self._get_service_instance(service_ref['uuid'])
+            ip_refs = self._get_right_ip_refs_from_service(service)
+            return [ip_ref['uuid'] for ip_ref in ip_refs]
+
+    def _get_right_ip_refs_from_service(self, service):
+        # Since ServiceInstance.right_ip_address is deprecated, we need
+        # to get the value manually by traversing TF's data structures.
+        ip_refs = []
+        for vm in self._get_vms_from_service_instances_back_refs(service):
+            for vmi in self._get_vmis_from_virtual_machines_back_refs(vm):
+                if self._service_interface_is_of_right_type(vmi):
+                    ip_refs.extend(vmi['instance_ip_back_refs'])
+        return ip_refs
+
+    def _get_vms_from_service_instances_back_refs(self, service_instance):
+        return self._get_resources_by_objects_refs(
+            'virtual-machine', service_instance,
+            'virtual_machine_back_refs')
+
+    def _get_vmis_from_virtual_machines_back_refs(self, virtual_machine):
+        return self._get_resources_by_objects_refs(
+            'virtual-machine-interface', virtual_machine,
+            'virtual_machine_interface_back_refs')
+
+    def _get_resources_by_objects_refs(self, resource_type, obj, ref_type):
+        resources = [self._get_resource(resource_type, ref['uuid'])
+                     for ref in obj[ref_type]]
+        return [res for res in resources if res is not None]
+
+    def _get_service_instance(self, service_id):
+        return self._get_resource('service-instance', service_id)
+
+    def _get_instance_ip(self, iip_id):
+        return self._get_resource('instance-ip', iip_id)
+
+    def _get_router(self, router_id):
+        return self._get_resource('logical-router', router_id)
+
+    def _get_resource(self, res_type, res_id):
+        status_code, resource = self.rest_driver.get_resource(res_type, None,
+                                                              res_id)
+        if status_code == http_status.ok:
+            return resource[res_type]
+        return None
+
+    @staticmethod
+    def _not_a_snat_service(service_ref):
+        return not any("snat_" in name for name in service_ref['to'])
+
+    @staticmethod
+    def _service_interface_is_of_right_type(vmi):
+        return vmi.get('virtual_machine_interface_properties', {}
+                       ).get('service_interface_type', '') == 'right'
---
+++ networking_opencontrail/tests/unit/l3/test_snat_synchronizer.py
@@ -0,0 +1,213 @@
+# Copyright (c) 2019 OpenStack Foundation
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+import logging
+import mock
+
+from networking_opencontrail.l3 import snat_synchronizer
+
+from neutron.tests.unit.extensions import base as test_extensions_base
+
+
+class TungstenFabricSNATTestCases(test_extensions_base.ExtensionTestCase):
+    def setUp(self):
+        super(TungstenFabricSNATTestCases, self).setUp()
+        logging.disable(logging.CRITICAL)
+
+        self.tf_helper = self._get_tf_helper()
+
+        self.rest_driver_mock = RestDriverMock()
+        self.tf_helper.rest_driver = self.rest_driver_mock
+
+    def tearDown(self):
+        super(TungstenFabricSNATTestCases, self).tearDown()
+        logging.disable(logging.NOTSET)
+
+    def test_get_snat_ips(self):
+        snat_ips = self.tf_helper.get_snat_ips(
+            '9e824605-64b4-4f29-bbbb-4a537dea9f4c')
+
+        self.assertIn('10.10.10.10', snat_ips)
+        self.assertNotIn('10.10.10.11', snat_ips)
+
+    @mock.patch("retrying.time.sleep")
+    def test_retry_get_snat_ips(self, _):
+        self.rest_driver_mock.get_exceptions = [KeyError()]
+
+        snat_ips = self.tf_helper.get_snat_ips(
+            '9e824605-64b4-4f29-bbbb-4a537dea9f4c')
+
+        self.assertIn('10.10.10.10', snat_ips)
+        self.assertNotIn('10.10.10.11', snat_ips)
+
+    @mock.patch("networking_opencontrail.drivers.drv_opencontrail.cfg")
+    def _get_tf_helper(self, cfg):
+        cfg.CONF.APISERVER.use_ssl = False
+        cfg.CONF.APISERVER.insecure = True
+        cfg.CONF.APISERVER.cafile = None
+        return snat_synchronizer.TFHelper()
+
+
+class SnatSynchronizerTestCases(test_extensions_base.ExtensionTestCase):
+    def setUp(self):
+        super(SnatSynchronizerTestCases, self).setUp()
+        logging.disable(logging.CRITICAL)
+
+        self.snat_sync = self._get_snat_synchronizer()
+        self.neutron = mock.Mock()
+        self.tf = mock.Mock()
+        self.snat_sync.neutron_helper = self.neutron
+        self.snat_sync.tf_helper = self.tf
+
+        self.context = mock.Mock()
+        self.router = {'id': '9e824605-64b4-4f29-bbbb-4a537dea9f4c',
+                       'external_gateway_info': mock.Mock()}
+
+    def tearDown(self):
+        super(SnatSynchronizerTestCases, self).tearDown()
+        logging.disable(logging.NOTSET)
+
+    def test_create_snat_interface(self):
+        self.tf.get_snat_ips.return_value = ['10.10.10.10']
+        self.neutron.get_snat_interfaces.return_value = []
+
+        self.snat_sync.sync_snat_interfaces(self.context,
+                                            self.router['id'],
+                                            self.router)
+
+        self.neutron.create_snat_interface.assert_called_once_with(
+            self.context,
+            self.router['external_gateway_info'],
+            self.router['id'],
+            '10.10.10.10'
+        )
+
+    def test_remove_snat_interface(self):
+        snat_interface = {'fixed_ips': [{'ip_address': '10.10.10.10'}]}
+        self.neutron.get_snat_interfaces.return_value = [snat_interface]
+        self.tf.get_snat_ips.return_value = []
+
+        self.snat_sync.sync_snat_interfaces(self.context,
+                                            self.router['id'],
+                                            self.router)
+
+        self.neutron.delete_snat_interface.assert_called_once_with(
+            self.context, snat_interface)
+
+    @mock.patch("networking_opencontrail.drivers.drv_opencontrail.cfg")
+    def _get_snat_synchronizer(self, cfg):
+        cfg.CONF.APISERVER.use_ssl = False
+        cfg.CONF.APISERVER.insecure = True
+        cfg.CONF.APISERVER.cafile = None
+        snat_sync = snat_synchronizer.SnatSynchronizer()
+        return snat_sync
+
+
+class RestDriverMock(object):
+    def __init__(self):
+        self._exceptions = iter([])
+
+    def get_resource(self, res_type, query, id):
+        if res_type != 'logical-router':
+            try:
+                raise next(self._exceptions)
+            except StopIteration:
+                pass
+        return self._get_resource(res_type, query, id)
+
+    def _get_resource(self, res_type, query, id):
+        resource_infos = {
+            'logical-router': self._get_router_rest(),
+            'service-instance': self._get_service_instance_rest(),
+            'virtual-machine': self._get_virtual_machine_rest(),
+            'virtual-machine-interface':
+                self._get_virtual_machine_interface_rest(id),
+            'instance-ip': self._get_instance_ip(id),
+        }
+        content = resource_infos.get(res_type, {})
+        status_code = 200
+        return status_code, content
+
+    @staticmethod
+    def _get_router_rest():
+        return {'logical-router': {
+            'service_instance_refs': [{
+                'uuid': '32adb91e-3e32-4064-a3cf-c4e631a247a3',
+                'to': ['snat_'],
+            }]
+        }}
+
+    @staticmethod
+    def _get_service_instance_rest():
+        return {'service-instance': {
+            'virtual_machine_back_refs': [{
+                'uuid': 'cde8f476-8342-4792-b020-82b40446b18a',
+            }]
+        }}
+
+    @staticmethod
+    def _get_virtual_machine_rest():
+        return {'virtual-machine': {
+            'virtual_machine_interface_back_refs': [
+                {'uuid': '0f716eb5-2820-4edc-921d-bdd6f8670733'},
+                {'uuid': 'fb33517f-eddb-4a4b-bf54-05226a0956c9'}
+            ]}
+        }
+
+    @staticmethod
+    def _get_virtual_machine_interface_rest(uuid):
+        interfaces = {
+            '0f716eb5-2820-4edc-921d-bdd6f8670733': {
+                'virtual-machine-interface': {
+                    'instance_ip_back_refs': [{
+                        'uuid': 'a905c5e1-7564-44bb-9edc-436caad7d7a2'
+                    }],
+                    'virtual_machine_interface_properties': {
+                        'service_interface_type': 'right',
+                    }
+                }},
+            'fb33517f-eddb-4a4b-bf54-05226a0956c9': {
+                'virtual-machine-interface': {
+                    'instance_ip_back_refs': [{
+                        'uuid': 'af32f3d9-708e-444b-b0dc-3ed3a60d4e8a'
+                    }],
+                    'virtual_machine_interface_properties': {
+                        'service_interface_type': 'left',
+                    }
+                }},
+        }
+        return interfaces.get(uuid)
+
+    @staticmethod
+    def _get_instance_ip(uuid):
+        ips = {
+            'a905c5e1-7564-44bb-9edc-436caad7d7a2': {
+                'instance-ip': {
+                    'instance_ip_address': '10.10.10.10',
+                }},
+            'af32f3d9-708e-444b-b0dc-3ed3a60d4e8a': {
+                'instance-ip': {
+                    'instance_ip_address': '10.10.10.11',
+                }},
+        }
+        return ips.get(uuid)
+
+    @property
+    def exceptions(self):
+        return [exc for exc in self._exceptions]
+
+    @exceptions.setter
+    def exceptions(self, exceptions):
+        self._exceptions = iter(exceptions)
