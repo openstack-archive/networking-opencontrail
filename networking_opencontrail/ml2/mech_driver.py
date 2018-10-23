0bf8fb84356e5a62fd296cdf4dd309d68b7bc350
Adrian Szczepański <adrian.szczepanski@codilime.com>
2018-10-23 16:10:08 +0200

    Create port for snat
    Create port for SNAT to prevent IP address collisions
    Add SNAT handling in L3 flavor
    
    Co-authored-by: Kamil Mankowski <kamil.mankowski@codilime.com>
    Co-authored-by: Maciej Jagiello <maciej.jagiello@codilime.com>
    
    Change-Id: I7c0cbbd9f3c5045d31f9bb66450552a08f424f32

--- networking_opencontrail/ml2/mech_driver.py
+++ networking_opencontrail/ml2/mech_driver.py
@@ -18,12 +18,16 @@ from oslo_log import log as logging
 import networking_opencontrail.drivers.drv_opencontrail as drv
 from neutron_lib.plugins.ml2 import api
 
+from networking_opencontrail.l3 import snat_synchronizer
 from networking_opencontrail.ml2 import opencontrail_sg_callback
 from networking_opencontrail.ml2 import subnet_dns_integrator
 
 LOG = logging.getLogger(__name__)
-OMIT_DEVICES_TYPES = ["network:floatingip",
-                      subnet_dns_integrator.TF_DNS_DEVICE_OWNER]
+OMIT_DEVICES_TYPES = [
+    "network:floatingip",
+    snat_synchronizer.TF_SNAT_DEVICE_OWNER,
+    subnet_dns_integrator.TF_DNS_DEVICE_OWNER,
+]
 
 
 class OpenContrailMechDriver(api.MechanismDriver):
